eff648a5c99449cddd9d87a2a255d26d0f0ec7f7
Avnee Dadhich <avneedadhich38@gmail.com>
2025-04-13 23:36:54 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//though by default,treat all JS code as newer version
+//alert(3+3) // we are using nodejs ,not browser
+//; not necessary nut to separate statements
+console.log(3+
+    3)//runs but not a good practice
+
+let name="Avnee"    //string   "prefer"
+let age=18        //int->2^53 , bigint
+let isLoggedIn=false;//boolean   true/false
+//null->standalone value      //khali h
+//undefined->value not assigned yet
+//symbol->unique
+//these were primitive datatypes
+
+//object
+console.log(typeof "avnee")
+console.log(typeof age)//number
+console.log(typeof null)//obj as op     //undefined ka type is undefined only
+//so undefined is a type in itself but null is an object
